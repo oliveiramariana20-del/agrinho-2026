428c7c1683c8c96c8404b33c10eacb0295ca189a
oliveiramariana20-del <oliveira.mariana20@escola.pr.gov.br>
2026-06-03 11:13:31 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", () => {
+    alert(
+        "🌱 O Agro Sustentável promove a produção agrícola com respeito ao meio ambiente e às futuras gerações!"
+    );
+});
